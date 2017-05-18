7397be547df038278c6f67bad2a04cac31a0b2af
iamrahulmehta <rahul.mehta749@gmail.com>
2017-05-18 19:26:32 +0530

    How to log in and out from facebook using id
    import org.openqa.selenium.By;
    import org.openqa.selenium.WebDriver;
    import org.openqa.selenium.firefox.FirefoxDriver;
    
    public class Facebook {
    
    	public static void main(String[] args) throws InterruptedException {
    
    String url=System.getProperty("user.dir")+"\\src\\geckodriver.exe";
    String drivepath=System.setProperty("webdriver.gecko.driver", url);
    WebDriver driver=new FirefoxDriver();
    driver.get("http://facebook.com");	
    driver.findElement(By.id("email")).sendKeys("EMail or user name");
    driver.findElement(By.id("pass")).sendKeys("Password");
    driver.findElement(By.id("u_0_q")).click();
    Thread.sleep(15000);
    driver.findElement(By.id("userNavigationLabel")).click();
    Thread.sleep(3000);
    driver.findElement(By.partialLinkText("Log out")).click();
    
    
    
    	}
    
    }

--- Facebook.java
+++ Facebook.java
@@ -4,17 +4,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Facebook {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 String url=System.getProperty("user.dir")+"\\src\\geckodriver.exe";
 String drivepath=System.setProperty("webdriver.gecko.driver", url);
 WebDriver driver=new FirefoxDriver();
 driver.get("http://facebook.com");	
-driver.findElement(By.id("email")).sendKeys("Testing");
-driver.findElement(By.id("pass")).sendKeys("Testing");
-//driver.findElement(By.id("u_0_q")).submit();
-//Or you can click instead of submit. 
+driver.findElement(By.id("email")).sendKeys("EMail or user name");
+driver.findElement(By.id("pass")).sendKeys("Password");
 driver.findElement(By.id("u_0_q")).click();
+Thread.sleep(15000);
+driver.findElement(By.id("userNavigationLabel")).click();
+Thread.sleep(3000);
+driver.findElement(By.partialLinkText("Log out")).click();
 
 
 
