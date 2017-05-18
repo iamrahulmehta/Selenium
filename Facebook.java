72e35b7a7a86713aad3acebec30cf8886a9ae0bc
iamrahulmehta <rahul.mehta749@gmail.com>
2017-05-18 18:53:28 +0530

    How to log in to Facebook?
    import org.openqa.selenium.By;
    import org.openqa.selenium.WebDriver;
    import org.openqa.selenium.firefox.FirefoxDriver;
    
    public class Facebook {
    
    	public static void main(String[] args) {
    
    String url=System.getProperty("user.dir")+"\\src\\geckodriver.exe";
    String drivepath=System.setProperty("webdriver.gecko.driver", url);
    WebDriver driver=new FirefoxDriver();
    driver.get("http://facebook.com");	
    driver.findElement(By.id("email")).sendKeys("Testing");
    driver.findElement(By.id("pass")).sendKeys("Testing");
    //driver.findElement(By.id("u_0_q")).submit();
    //Or you can click instead of submit. 
    driver.findElement(By.id("u_0_q")).click();
    
    
    
    	}
    
    }

--- Facebook.java
+++ Facebook.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,8 +9,15 @@ public class Facebook {
 String url=System.getProperty("user.dir")+"\\src\\geckodriver.exe";
 String drivepath=System.setProperty("webdriver.gecko.driver", url);
 WebDriver driver=new FirefoxDriver();
-	
-		
+driver.get("http://facebook.com");	
+driver.findElement(By.id("email")).sendKeys("Testing");
+driver.findElement(By.id("pass")).sendKeys("Testing");
+//driver.findElement(By.id("u_0_q")).submit();
+//Or you can click instead of submit. 
+driver.findElement(By.id("u_0_q")).click();
+
+
+
 	}
 
 }
